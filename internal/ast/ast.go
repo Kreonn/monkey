34a1b0c4da77bedd12ac953fd61e8d7603493e3e
Kreonn <christopher.georget@protonmail.com>
2024-01-17 22:19:14 +0000

    feat: Add ExpressionStatement (and String method)

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -1,10 +1,15 @@
 package ast
 
-import "github.com/Kreonn/monkey/internal/token"
+import (
+	"bytes"
+
+	"github.com/Kreonn/monkey/internal/token"
+)
 
 // Node represents a single Node of the AST
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement represents a Node that does not produce a value
@@ -32,6 +37,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // LetStatement represents a variable assignation Statement
 type LetStatement struct {
 	Token token.Token
@@ -43,6 +58,22 @@ func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
 
 // ReturnStatement represents a return Statement
@@ -57,6 +88,40 @@ func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement represents an evaluable Statement
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
 // Identifier represents an user-inputted Expression
 type Identifier struct {
 	Token token.Token
@@ -67,4 +132,8 @@ func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
